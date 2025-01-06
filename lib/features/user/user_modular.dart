becb08aebdb7a6d1b1f2ed0988760f93f3fb3c97
watchakorn-18k <wk18k@proton.me>
2025-01-07 01:32:12 +0700

    style : clean code

---
+++ lib/features/user/user_modular.dart
@@ -0,0 +1,25 @@
+import 'package:clean_architecture_app/features/user/data/datasources/remote/user_remote_datasource.dart';
+import 'package:clean_architecture_app/features/user/data/repositories/user_repository_impl.dart';
+import 'package:clean_architecture_app/features/user/domain/repositories/user_repository.dart';
+import 'package:clean_architecture_app/features/user/domain/usecases/get_user.dart';
+import 'package:clean_architecture_app/features/user/domain/usecases/get_user_all.dart';
+import 'package:clean_architecture_app/features/user/presentation/bloc/user_bloc.dart';
+import 'package:clean_architecture_app/injection_container.dart';
+
+class UserModule {
+  static void register() {
+    // Bloc
+    sl.registerFactory(() => UserBloc(getUser: sl(), getUserAll: sl()));
+
+    // Use cases
+    sl.registerLazySingleton(() => GetUser(sl()));
+    sl.registerLazySingleton(() => GetUserAll(sl()));
+
+    // Repository
+    sl.registerLazySingleton<UserRepository>(() => UserRepositoryImpl(sl()));
+
+    // Data sources
+    sl.registerLazySingleton<UserRemoteDataSource>(
+        () => UserRemoteDataSourceImpl(client: sl()));
+  }
+}
--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,32 +1,17 @@
 import 'package:clean_architecture_app/core/database/app_database.dart';
-import 'package:clean_architecture_app/features/user/data/datasources/remote/user_remote_datasource.dart';
-import 'package:clean_architecture_app/features/user/data/repositories/user_repository_impl.dart';
-import 'package:clean_architecture_app/features/user/domain/repositories/user_repository.dart';
-import 'package:clean_architecture_app/features/user/domain/usecases/get_user.dart';
-import 'package:clean_architecture_app/features/user/domain/usecases/get_user_all.dart';
-import 'package:clean_architecture_app/features/user/presentation/bloc/user_bloc.dart';
+
+import 'package:clean_architecture_app/features/user/user_modular.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
-  final db = AppDatabase();
+  // Register AppDatabase
+  sl.registerLazySingleton(() => AppDatabase());
 
-  // Bloc
-
-  sl.registerFactory(() => UserBloc(getUser: sl(), getUserAll: sl()));
-
-  // Use cases
-  sl.registerLazySingleton(() => GetUser(sl()));
-  sl.registerLazySingleton(() => GetUserAll(sl()));
-
-  // Repository
-  sl.registerLazySingleton<UserRepository>(() => UserRepositoryImpl(db));
-
-  // Data sources
-  sl.registerLazySingleton<UserRemoteDataSource>(
-      () => UserRemoteDataSourceImpl(client: sl()));
+  // Register UserModule
+  UserModule.register();
 
   // External
   sl.registerLazySingleton(() => http.Client());
